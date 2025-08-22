0d5e5c139e69ad261e068929916e40f5300e2eb4
olle stenson <olle.stenson@yahoo.com>
2025-08-22 11:40:35 +0200

    Documentation

--- scripts/config.py
+++ scripts/config.py
@@ -1,21 +1,24 @@
-# List of tickers to analyze
-TICKERS = ['BALD-B.ST', 'CORE-B.ST', 'SAGA-B.ST', 'CAST.ST']
-REFRESH_DAYS = 7
+"""
+Configuration file for the DCF analysis script.
+This file contains constants and configurations used throughout the script.
+"""
 
-GROWTH_RATE = 0.05          # 5% annual growth
-DISCOUNT_RATE = 0.10        # 10% discount rate (If not calculating WACC)
-TERMINAL_GROWTH = 0.02      # 2% perpetual growth
+TICKERS = ['BALD-B.ST', 'CORE-B.ST', 'SAGA-B.ST', 'CAST.ST']    # List of tickers to analyze
+REFRESH_DAYS = 7    # Number of days to refresh data
 
-# DCF variables
-YEARS = 5
-RISK_FREE_RETURN = 0.025
-MARKET_RETURN = 0.08
+GROWTH_RATE = 0.05          # 5% annual growth rate for projected free cash flows
+DISCOUNT_RATE = 0.10        # 10% discount rate for DCF calculations (if not using WACC)
+TERMINAL_GROWTH = 0.02      # 2% terminal growth rate for calculating terminal value
+
+YEARS = 5   # Number of years to project free cash flows
+RISK_FREE_RETURN = 0.025    # 2.5% risk-free return (e.g., from government bonds)
+MARKET_RETURN = 0.08    # 8% expected market return (e.g., from stock market index)
 
 # Database file location
-DB_PATH = r"../db/dcf.db"
-TEST_DB_PATH = r"../db/test_dcf.db"
+DB_PATH = r"../db/dcf.db"    # Main database file for DCF analysis
+TEST_DB_PATH = r"../db/test_dcf.db"     # Test database file for DCF analysis
 
-# Fetches the config + share_price at fiscal date
+# Configuration for fetching financial data from yfinance
 FETCH_CONFIG = {
     'cashflow': {
         'Free Cash Flow': 'fcf'
